0bc7109aa3af993fae5fbef957706f2be3b1eeb0
Greg Hinsley <greghinsley@Gregs-MacBook-Air.local>
2016-09-13 13:47:20 +0100

    adding jl demo

---
+++ src/test/scala/kraftwerk/jl.scala
@@ -0,0 +1,28 @@
+
+import scala.concurrent.duration._
+
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+
+class jl extends Simulation {
+
+	val httpProtocol = http
+		.baseURL("https://m.johnlewis.com")
+
+	val headers_0 = Map(
+		"Proxy-Connection" -> "keep-alive",
+		"Upgrade-Insecure-Requests" -> "1")
+
+	val scn = scenario("jl")
+		.exec(http("m.johnlewis.com")
+			.get("/")
+			.headers(headers_0))
+
+	setUp(
+		scn.inject(
+			rampUsers(10) over(10 seconds),
+			constantUsersPerSec(10) during(50 seconds)
+		)
+	).protocols(httpProtocol)
+}
